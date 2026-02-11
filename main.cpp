aad35e208393dce9f7e275c9d78ab7a8ad99e37b
WesleeVpcc <you@example.com>
2026-02-11 08:32:33 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+ File: classworkfeb11.cpp
+ Programmer: Weslee Smith
+ Date: 02 Feb 26
+ Requirements: Sum of two numbers. Write a program that stores the intergers 50 and 100 in variables,
+ and stores the sum of these two in a variable named sum.
+ Display the result.
+ 1. Create 3 varibles.
+ 2. Assign with values 50 and 100.
+ 3. Add the two varibles values and store the third
+ */
+
+#include <iostream>
+using namespace std;
+int main()
+{
+ int number1, number2, sum;
+ number1 = 50;
+ number2 = 100;
+ sum = number1 + number2;
+ cout << "The sum of " << number1 << " and " << number2 << " is " << sum << endl;
+}
